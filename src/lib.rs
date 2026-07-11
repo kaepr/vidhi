8a8ffec68e078fc8ecff3c0f0f1cdad3d4b27185
Shagun Agrawal <agrawalshagun07@gmail.com>
2026-07-11 22:51:38 +0530

    Add nice declarative macros to create facts/patterns.

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,30 @@ pub enum Atom {
     Number(isize),
 }
 
+impl From<isize> for Atom {
+    fn from(value: isize) -> Self {
+        Atom::Number(value)
+    }
+}
+
+impl From<&str> for Atom {
+    fn from(value: &str) -> Self {
+        Atom::String(value.to_string())
+    }
+}
+
 /// Represents a variable.
 #[derive(Debug, Clone, PartialEq, Hash)]
 pub struct Variable {
     pub name: String,
+}
+
+impl From<&str> for Variable {
+    fn from(value: &str) -> Self {
+        Variable {
+            name: value.to_string(),
+        }
+    }
 }
 
 /// Represents either a variable or a concrete value.
@@ -43,19 +63,116 @@ pub fn unify(pattern: &Pattern, fact: &Fact, bindings: &Bindings) -> Option<Bind
     todo!()
 }
 
+#[macro_export]
+macro_rules! atom {
+    ($lit:literal) => {
+        Atom::from($lit)
+    };
+    ($name:ident) => {
+        Atom::from(stringify!($name))
+    };
+}
+
+#[macro_export]
+macro_rules! variable {
+    ($name:ident) => {
+        Variable::from(stringify!($name))
+    };
+}
+
+#[macro_export]
+macro_rules! fact {
+    ($e: tt, $a: tt, $v: tt) => {
+        Fact {
+            entity: atom!($e),
+            attribute: atom!($a),
+            value: atom!($v),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! pattern {
+    (? $e:ident, $($rest:tt)*) => {
+        pattern!(@attr Term::Variable(variable!($e)), $($rest)*)
+    };
+    ($e:tt, $($rest:tt)*) => {
+        pattern!(@attr Term::Literal(atom!($e)), $($rest)*)
+    };
+    (@attr $entity:expr, ? $a:ident, $($rest:tt)*) => {
+        pattern!(@val $entity, Term::Variable(variable!($a)), $($rest)*)
+    };
+    (@attr $entity:expr, $a:tt, $($rest:tt)*) => {
+        pattern!(@val $entity, Term::Literal(atom!($a)), $($rest)*)
+    };
+    (@val $entity:expr, $attribute:expr, ? $v:ident) => {
+        Pattern {
+            entity: $entity,
+            attribute: $attribute,
+            value: Term::Variable(variable!($v))
+        }
+    };
+    (@val $entity:expr, $attribute:expr, $v:tt) => {
+        Pattern {
+            entity: $entity,
+            attribute: $attribute,
+            value: Term::Literal(atom!($v))
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use test_that::prelude::*;
 
     #[test_that::test]
-    fn test_fact() {
-        let fact = Fact {
-            entity: Atom::String("player".to_string()),
-            attribute: Atom::String("health".to_string()),
-            value: Atom::Number(10),
-        };
+    fn test_atom() {
+        let atom = atom!(10);
+        expect_that!(atom, eq(Atom::Number(10)));
 
-        expect_that!(fact.value, eq(Atom::Number(10)));
+        let atom = atom!(player);
+        expect_that!(atom, eq(Atom::String("player".to_string())));
+    }
+
+    #[test_that::test]
+    fn test_fact() {
+        let fact = fact!(player, health, 10);
+
+        expect_that!(
+            fact,
+            eq(Fact {
+                entity: Atom::String("player".to_string()),
+                attribute: Atom::String("health".to_string()),
+                value: Atom::Number(10),
+            })
+        );
+    }
+
+    #[test_that::test]
+    fn test_variable() {
+        let variable = variable!(e);
+        expect_that!(variable, eq(Variable::from("e")));
+
+    }
+
+    #[test_that::test]
+    fn test_pattern() {
+        let pattern = pattern!(?e, health, 10);
+        expect_that!(
+            pattern,
+            eq(Pattern {
+                entity: Term::Variable("e".into()),
+                attribute: Term::Literal("health".into()),
+                value: Term::Literal(10.into()),
+            }));
+        let pattern = pattern!(?e, ?a, ?v);
+        expect_that!(
+            pattern,
+            eq(Pattern {
+                entity: Term::Variable("e".into()),
+                attribute: Term::Variable("a".into()),
+                value: Term::Variable("v".into()),
+            }));
     }
 }
